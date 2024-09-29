9d9d4ca42b250b33287534f48ec17b9c6b705e12
Otaku Codes <himanshupapola.ph@gmail.com>
2024-09-29 10:07:08 +0530

    Update SumTest.java

--- src/test/java/SumTest.java
+++ src/test/java/SumTest.java
@@ -10,4 +10,16 @@ public class SumTest {
         Sum sum = new Sum();
         assertEquals(100, sum.add(50, 50), "The sum of 50 and 50 should be 100");
     }
+
+    @Test
+    public void testAddNegativeNumbers() {
+        Sum sum = new Sum();
+        assertEquals(-10, sum.add(-5, -5), "The sum of -5 and -5 should be -10");
+    }
+
+    @Test
+    public void testAddWithZero() {
+        Sum sum = new Sum();
+        assertEquals(50, sum.add(50, 0), "The sum of 50 and 0 should be 50");
+    }
 }
